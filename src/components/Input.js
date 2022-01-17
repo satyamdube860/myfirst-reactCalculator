19d442404042dae4d21e2d63382fc76c97241494
satyam dubey <Satyam.Dubey@zennioptical.com>
2022-01-17 17:23:23 +0530

    update

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React,{Component} from 'react';
+import './Input.css';
+
+class Input extends Component{
+render(){
+
+    return(
+<div className ="input">
+{this.props.children}
+</div>
+    );
+}
+}
+
+
+
+export default Input;
